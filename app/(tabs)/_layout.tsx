c12e68839645543b2343a544e62843c7067f1bf4
Ulas <ulasbyz4949@gmail.com>
2025-04-28 14:33:42 +0300

    added responsive scan tab button and improved tabbar structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,91 @@
-import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons'; // İconlar için
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Dimensions, Image, TouchableOpacity } from "react-native";
 
 export default function TabLayout() {
+  const { height } = Dimensions.get("window"); 
+  const isSmallDevice = height < 700; // Bazı küçük cihazlarda scan butonu fazla yukarıda kalmasın diye yaptım.
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "#28AF6E",
+        tabBarInactiveTintColor: "#AAAAAA",
+        tabBarStyle: {
+          borderTopWidth: 0,
+          height: 84,
+          paddingBottom: 10,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: "600",
+        },
       }}
     >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="diagnose"
+        options={{
+          tabBarLabel: "Diagnose",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={24} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="scan"
+        options={{
+          tabBarButton: ({ onPress }) => (
+            <TouchableOpacity
+              onPress={onPress}
+              style={{
+                width: 64,
+                height: 64,
+                borderRadius: 32,
+                backgroundColor: "#28AF6E",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "absolute",
+                bottom: isSmallDevice ? 10 : 30,
+                alignSelf: "center",
+                zIndex: 10,
+              }}
+            >
+              <Image
+                source={require("../../assets/images/ScanwithoutBG.png")}
+              />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mygarden"
+        options={{
+          tabBarLabel: "My Garden",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="leaf-outline" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/scan.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+const scan = () => {
+  return (
+    <SafeAreaView>
+      <Text>scan</Text>
+    </SafeAreaView>
+  )
+}
+
+export default scan
